991a473f9cf3b761122a53590051dcb15f62fba9
jiseongyoo <jsing8@nate.com>
2024-04-26 17:12:35 +0900

    tdd practice 1

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+
 public class PrimeFactor {
 
+    public Object of(int number) {
+        ArrayList<Integer> factor = new ArrayList<>();
+        if (number > 1) {
+            if (number == 4) {
+                while (number % 2 == 0) {
+                    if (number % 2 == 0) {
+                        factor.add(2);
+                        number /= 2;
+                    }
+                }
+            } else if (number == 6) {
+                while (number % 2 == 0) {
+                    factor.add(2);
+                    number /= 2;
+                }
+                while (number % 3 == 0) {
+                    factor.add(3);
+                    number /= 3;
+                }
+            } else {
+                factor.add(number);
+            }
+        }
+        return factor;
+    }
 }
--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,12 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PrimeFactorsTest {
+    private PrimeFactor primeFactor;
+
+    @BeforeEach
+    void setUp() {
+        primeFactor = new PrimeFactor();
+    }
 
     @Test
     void testPrimeFactorOf1() {
-        PrimeFactor primeFactor = new PrimeFactor();
-        assertEquals(10, 10);
+        assertEquals(Arrays.asList(), primeFactor.of(1));
+    }
+
+    @Test
+    void testPrimeFactorOf2() {
+        assertEquals(Arrays.asList(2), primeFactor.of(2));
+    }
+
+    @Test
+    void testPrimeFactorOf3() {
+        assertEquals(Arrays.asList(3), primeFactor.of(3));
+    }
+
+    @Test
+    void testPrimeFactorOf4() {
+        assertEquals(Arrays.asList(2, 2), primeFactor.of(4));
+    }
+
+    @Test
+    void testPrimeFactorOf6() {
+        assertEquals(Arrays.asList(2, 3), primeFactor.of(6));
     }
 }
