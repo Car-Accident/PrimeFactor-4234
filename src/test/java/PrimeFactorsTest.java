e4adc95f044caea26d89fc93e12250383fe68604
jiseongyoo <jsing8@nate.com>
2024-04-26 16:54:17 +0900

    first commit

---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PrimeFactorsTest {
+
+    @Test
+    void testPrimeFactorOf1() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(10, 10);
+    }
+}
